a6f1742d8309c17b0252bdef9b09492dc2b0963b
Hector Damas <hectorgabrieldm@hotmail.com>
2025-05-29 10:17:40 -0400

    tenancy

--- routes/tenant.php
+++ routes/tenant.php
@@ -24,9 +24,17 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
-    });
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+        Auth::routes();
+
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+        Route::group(['middleware' => [ 'auth' ]], function(){
+
+        });    
 });
 
 
