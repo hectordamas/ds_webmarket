b9fc41f258793d49d91169e08f6301dd19bc9266
Hector Damas <hectorgabrieldm@hotmail.com>
2025-05-29 17:01:02 -0400

    dswebmarket

--- routes/tenant.php
+++ routes/tenant.php
@@ -5,23 +5,21 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+use App\Http\Controllers\Auth\TenantLoginController;
 
 Route::middleware([
     'web',
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-        Route::get('/', function () {
-            return view('catalogo');
-        });
+    Route::get('/', function () {
+        return view('catalogo');
+    });
 
-        Auth::routes();
-
-        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-        Route::group(['middleware' => [ 'auth' ]], function(){
-
-        });    
+    Route::get('/login', [TenantLoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [TenantLoginController::class, 'login']);
+    Route::post('/logout', [TenantLoginController::class, 'logout'])->name('logout');
+ 
 });
 
 
