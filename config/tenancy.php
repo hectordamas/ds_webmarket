80d4e4d6e02ee9e9b39a5ac4f599f15f7c5fae4c
Hector Damas <hectorgabrieldm@hotmail.com>
2025-07-11 09:48:16 -0400

    Stancl\Tenancy\Bootstrappers\DatabaseTenancyBootstrapper::class => [         'ensure_tenant_database_exists' => false,     ],

--- config/tenancy.php
+++ config/tenancy.php
@@ -30,9 +30,7 @@ return [
      * To configure their behavior, see the config keys below.
      */
     'bootstrappers' => [
-        Stancl\Tenancy\Bootstrappers\DatabaseTenancyBootstrapper::class => [
-            'ensure_tenant_database_exists' => false,
-        ],        
+        Stancl\Tenancy\Bootstrappers\DatabaseTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\QueueTenancyBootstrapper::class,
