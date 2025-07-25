47797c1a7a0162fc6cb72e78bcacb076651c15f5
Hector Damas <hectorgabrieldm@hotmail.com>
2025-07-25 15:21:35 -0400

    activo users

--- app/Http/Controllers/Auth/TenantLoginController.php
+++ app/Http/Controllers/Auth/TenantLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Auth;
 
 class TenantLoginController extends Controller
