df6fe2b55860758fbd032ac762c512eab56c3ad1
Hector Damas <hectorgabrieldm@hotmail.com>
2025-07-28 14:32:08 -0400

    idioma

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'failed'   => 'Usuario o contraseña inválida.',
+    'failed'   => 'Correo Electrónico o Contraseña inválida.',
     'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Demasiados intentos. Intenta de nuevo en :seconds segundos.',
 ];
