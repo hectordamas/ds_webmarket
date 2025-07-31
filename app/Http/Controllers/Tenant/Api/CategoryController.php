c26d948d8c14bb3da5780db09e2eac47e570b8b2
Hector Damas <hectorgabrieldm@hotmail.com>
2025-07-31 11:27:41 -0400

    validate

--- app/Http/Controllers/Tenant/Api/CategoryController.php
+++ app/Http/Controllers/Tenant/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tenant\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tenant\{Category, Product};
+use Validator;
 
 class CategoryController extends Controller
 {
