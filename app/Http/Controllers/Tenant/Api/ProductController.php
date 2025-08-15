cc2d4d43a8e0dc32e1f103ddf2ff67a5290fd0c8
Hector Damas <hectorgabrieldm@hotmail.com>
2025-08-15 14:35:13 -0400

    error

--- app/Http/Controllers/Tenant/Api/ProductController.php
+++ app/Http/Controllers/Tenant/Api/ProductController.php
@@ -10,54 +10,60 @@ use Illuminate\Support\Facades\Validator;
 class ProductController extends Controller
 {
     public function store(Request $request){
-	    $data = [];
-        $datosReq = $request->all(); 
+        try {
 
-        for ($i = 0; $i < count($datosReq); $i++) {
-            $data['CodProd'][$i] = @$datosReq[$i]['CodProd'];
-			$data['Descrip'][$i] = @$datosReq[$i]['Descrip'];
-			$data['Activo'][$i] = @$datosReq[$i]['Activo'];
-			$data['CodInst'][$i] = @$datosReq[$i]['CodInst'];
-            $data['Evento'][$i] = @$datosReq[$i]['Evento'];
-            $data['Existen'][$i] = @$datosReq[$i]['Existen'];
-            $data['Precio'][$i] = @$datosReq[$i]['Precio'];
-            $data['Descripcion'][$i] = @$datosReq[$i]['Descripcion'];
-		}
+	        $data = [];
+            $datosReq = $request->all(); 
 
-        for ($i = 0; $i < count($data['CodProd']); $i++) {
-            $sku = $item['CodProd'][$i]  ?? null;
-            $evento = $item['Evento'][$i] ?? null;
+            for ($i = 0; $i < count($datosReq); $i++) {
+                $data['CodProd'][$i] = @$datosReq[$i]['CodProd'];
+		    	$data['Descrip'][$i] = @$datosReq[$i]['Descrip'];
+		    	$data['Activo'][$i] = @$datosReq[$i]['Activo'];
+		    	$data['CodInst'][$i] = @$datosReq[$i]['CodInst'];
+                $data['Evento'][$i] = @$datosReq[$i]['Evento'];
+                $data['Existen'][$i] = @$datosReq[$i]['Existen'];
+                $data['Precio'][$i] = @$datosReq[$i]['Precio'];
+                $data['Descripcion'][$i] = @$datosReq[$i]['Descripcion'];
+		    }
 
-            if (!$sku || !$evento) {
-                continue;
-            }
-        
-            $product = Product::where('sku', $sku)->first();
-        
-            if ($evento == 'D') {
-                if ($product) {
-                    $product->delete();
-                }
-            } else {
-                if (!$product) {
-                    $product = new Product();
-                    $product->sku = $sku;
+            for ($i = 0; $i < count($data['CodProd']); $i++) {
+                $sku = $item['CodProd'][$i]  ?? null;
+                $evento = $item['Evento'][$i] ?? null;
+
+                if (!$sku || !$evento) {
+                    continue;
                 }
             
-                $product->sku = $item['CodProd'][$i];
-                $product->name = $item['Descrip'][$i];
-                $product->description = $item['Descripcion'][$i];
-                $product->codinst = $item['CodInst'][$i];
-                $product->stock = $item['Existen'][$i];
-                $product->active = $item['Activo'][$i];
-                $product->price = $item['Precio'][$i];
-                $product->save();
+                $product = Product::where('sku', $sku)->first();
+            
+                if ($evento == 'D') {
+                    if ($product) {
+                        $product->delete();
+                    }
+                } else {
+                    if (!$product) {
+                        $product = new Product();
+                        $product->sku = $sku;
+                    }
+                
+                    $product->sku = $item['CodProd'][$i];
+                    $product->name = $item['Descrip'][$i];
+                    $product->description = $item['Descripcion'][$i];
+                    $product->codinst = $item['CodInst'][$i];
+                    $product->stock = $item['Existen'][$i];
+                    $product->active = $item['Activo'][$i];
+                    $product->price = $item['Precio'][$i];
+                    $product->save();
+                }
             }
-        }
 
-        return response()->json([
-            'success' => true
-        ]);
+            return response()->json([
+                'success' => true
+            ]);
+        } catch (\Exception $e) {
+			return response()->json([$e->getMessage(), 'Linea ' . $e->getLine()]);
+		}
+
     }
 
 }
